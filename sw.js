fb7dd4952781ef6cc0115799b6b63d930afb0ac2
AnibalePalermo <enzo.diioia52@gmail.com>
2026-08-16 13:29:07 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='puglia-travel-v11';
+const CACHE='puglia-travel-v12';
 const STATIC=['./','./index.html','./manifest.json','./icons/icon-192.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{})));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
