0ea6b59432c5d14c90cbdc61b827ead35d15dea5
AnibalePalermo <enzo.diioia52@gmail.com>
2026-09-02 17:21:49 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='travel-app-v18-1';
+const CACHE='travel-app-v19';
 const STATIC=["./", "./index.html", "./images/alberobello.jpg", "./images/altamura.jpg", "./images/bari.jpg", "./images/barletta-trani.jpg", "./images/brindisi.jpg", "./images/departure.jpg", "./images/galatina.jpg", "./images/gallipoli.jpg", "./images/lecce.jpg", "./images/locorotondo.jpg", "./images/maglie.jpg", "./images/matera.jpg", "./images/monopoli-polignano.jpg", "./images/open-flexible.jpg", "./images/otranto.jpg", "./images/porto-cesareo.jpg", "./images/rest-day.jpg", "./images/transfer.jpg"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{})));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
